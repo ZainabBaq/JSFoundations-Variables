c075bf80e41cc21e8f2a5c91fed7a443b841d5fe
ZainabBaq <52189918+ZainabBaq@users.noreply.github.com>
2021-02-02 16:18:13 +0300

    solvedgit add .

---
+++ story.js
@@ -0,0 +1,5 @@
+let name = "Zainab"
+let age = 20
+let hobby = "Painting"
+console.log(`Mr./Mrs. ${name}, in 33 years, you will be ${age+33} years old. By then you'll have decades of ${hobby} practice!`)
+
