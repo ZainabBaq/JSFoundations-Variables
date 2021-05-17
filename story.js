857defaca79d65082f7eabaafb66f09e2e452d34
ZainabBaq <52189918+ZainabBaq@users.noreply.github.com>
2021-05-17 16:13:14 +0300

    solved

--- story.js
+++ story.js
@@ -1,5 +1,9 @@
-let name = "Zainab"
-let age = 20
-let hobby = "Painting"
-console.log(`Mr./Mrs. ${name}, in 33 years, you will be ${age+33} years old. By then you'll have decades of ${hobby} practice!`)
+let name = "Zainab";
+let age = 20;
+let hobby = "Coding";
 
+console.log(
+  `Mr./Mrs ${name}, in 33 years, you will be ${
+    age + 33
+  } years old. By then you'll have decades of ${hobby} practice!`
+);
